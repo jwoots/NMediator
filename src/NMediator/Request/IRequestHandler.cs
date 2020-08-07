e532ed21b2dc4b3394f32f7f4518e1174f0557fb
woetsjo <woetsjo@FR09278644D>
2020-08-07 10:04:36 +0200

    commit

---
+++ src/NMediator/Request/IRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NMediator.Request
+{
+    public interface IRequestHandler
+    {
+        TResult Handle<TRequest, TResult>(TRequest request);
+    }
+}
