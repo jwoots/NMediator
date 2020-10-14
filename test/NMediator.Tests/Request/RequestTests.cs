0c491f13bffa25815d388aac3157fbb6cf769cc9
joffrey <joffrey.norsys@outlook.fr>
2020-10-14 21:41:35 +0200

    add test

--- test/NMediator.Tests/Request/RequestTests.cs
+++ test/NMediator.Tests/Request/RequestTests.cs
@@ -3,6 +3,7 @@ using NMediator.Activator;
 using NMediator.Configuration;
 using NMediator.Request;
 using NMediator.Result;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -30,6 +31,25 @@ namespace NMediator.Tests.Request
 
             //ASSERT
             result.Data.Should().Be("Hello World jwoots");
+        }
+
+        [Fact]
+        public async Task Execute_must_throw_InvalidOperationException_when_no_handler_registered()
+        {
+            var config = new MediatorConfiguration();
+            var activator = new SimpleHandlerActivator();
+
+            config.WithActivator(activator)
+                .Request(r => r.ExecuteWithInProcess());
+
+            BaseConfiguration.Configure(config);
+
+            //ACT
+            var processor = config.Container.Get<IRequestProcessor>();
+            Func<Task<RequestResult<string>>> action = () => processor.Execute<MyRequest, string>(new MyRequest() { Name = "jwoots" });
+
+            //ASSERT
+            await action.Should().ThrowAsync<InvalidOperationException>().WithMessage($"there is no handler instance for type {typeof(IRequestHandler<MyRequest,string>)}");
         }
 
         class MyRequest : IRequest<string>
